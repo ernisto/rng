48c3aa79a44e951388a6750f22a3902fae5f479b
cayasde <mydev.nice@gmail.com>
2026-05-10 23:28:38 -0400

    feat: support rbxts

---
+++ index.d.ts
@@ -0,0 +1,61 @@
+declare namespace rng {
+	type weight = number;
+	type seed = number | string | buffer;
+	type weight_map<K extends string = string> = Record<K, weight>;
+
+	interface helper {
+		number(this: void): number;
+		number(this: void, max: number): number;
+
+		step(this: void, max: number, step?: number): number;
+		range(this: void, min: number, max: number, step?: number): number;
+
+		int(this: void, max: number): number;
+		int_range(this: void, min: number, max: number, step?: number): number;
+
+		vector(this: void): vector;
+		vector(this: void, max: number): vector;
+		vector(this: void, x: number, y: number, z?: number, step?: number): vector;
+		vector_range(this: void, min: vector, max: vector, step?: vector): vector;
+		direction(this: void): vector;
+
+		buffer(this: void, count: number, target?: buffer, offset?: number): buffer;
+		truth(this: void, chance: number): boolean;
+		pass(this: void, chance: number): boolean;
+		skip(this: void, chance: number): boolean;
+
+		write_shuffle<T>(this: void, mut_arr: T[]): T[];
+		key_by_weight<K extends string>(this: void, key_weights: weight_map<K>): K;
+		value<T>(this: void, arr: ReadonlyArray<T>): T;
+		key<T extends object>(this: void, map: T): keyof T;
+	}
+
+	interface api {
+		new_secure: (this: void, seed?: seed, salt?: buffer) => helper;
+		custom: (this: void, generator: () => number) => helper;
+		new: (this: void, seed?: seed) => helper;
+
+		same_iter_order<K extends string>(this: void, key_weights: weight_map<K>): weight_map<K>;
+		rarest_keys<K extends string>(this: void, key_weights: weight_map<K>): K[];
+
+		vector_range: helper["vector_range"];
+		direction: helper["direction"];
+		vector: helper["vector"];
+		number: helper["number"];
+		range: helper["range"];
+		step: helper["step"];
+
+		buffer: helper["buffer"];
+		truth: helper["truth"];
+		pass: helper["pass"];
+		skip: helper["skip"];
+
+		write_shuffle: helper["write_shuffle"];
+		key_by_weight: helper["key_by_weight"];
+		value: helper["value"];
+		key: helper["key"];
+	}
+}
+
+declare const rng: rng.api;
+export = rng;
